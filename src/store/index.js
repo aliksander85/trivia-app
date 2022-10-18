1d583ac28f47d37ee654e05a2efa9619bb9555c1
Alexander Potapov <potapov.av.85@gmail.com>
2022-10-18 12:00:36 +0400

    [refactor]: set reducer to global namspace

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 import categoryReducer from './categorySlice';
 import requestParamsReducer from './requestParamsSlice';
+import questionsReducer from './questionsSlice';
 
 export default configureStore({
 	reducer: {
 		category: categoryReducer,
 		requestParams: requestParamsReducer,
+		questions: questionsReducer,
 	},
 });
